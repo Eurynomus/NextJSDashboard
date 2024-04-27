d3d56e7436c7ab18e236d3edd71c7ab26c8942cc
Eurynomus <45533743+Eurynomus@users.noreply.github.com>
2024-04-27 21:46:40 +0300

    Fixing deployment 3

--- middleware.js
+++ middleware.js
@@ -1,13 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  // '/'
+  '/dashboard(.*)'
 ]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
-}, { debug: true });
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
